212a06e4313193e3f0e1d0513b2ff522c058403d
ddhira123 <dhana_dhira@apps.ipb.ac.id>
2020-11-12 11:25:06 +0700

    Added workbox implementation

--- service-worker.js
+++ service-worker.js
@@ -1,98 +1,173 @@
+/* eslint-disable no-undef */
 /* eslint-disable func-names */
 /* eslint-disable eqeqeq */
 /* eslint-disable no-console */
 /* eslint-disable consistent-return */
 /* eslint-disable array-callback-return */
 /* eslint-disable no-restricted-globals */
-const CACHE_NAME = 'eplupdate';
-const urlsToCache = [
-	'/',
-	'/nav.html',
-	'/index.html',
-	'/teamDetails.html',
-	'/pages/home.html',
-	'/pages/about.html',
-	'/pages/favorites.html',
-	'/js/materialize.min.js',
-	'/js/nav.js',
-	'/js/notification.js',
-	'/js/pageLoader.js',
-	'/js/register_sw.js',
-	'/js/saved.js',
-	'/js/standingsApi.js',
-	'/js/teamDetails.js',
-	'/js/teamDetailsApi.js',
-	'/js/db.js',
-	'/js/idb.js',
-	'/css/materialize.min.css',
-	'/css/styles.css',
-	'/css/arrow.css',
-	'/img/bg-landing.jpg',
-	'/img/icon/logo-512x512.png',
-	'/img/icon/android-icon-192x192-dunplab-manifest-20202.png',
-	'/img/icon/apple-icon-180x180-dunplab-manifest-20202.png',
-	'/img/icon/apple-icon-152x152-dunplab-manifest-20202.png',
-	'/img/icon/apple-icon-144x144-dunplab-manifest-20202.png',
-	'/img/icon/apple-icon-120x120-dunplab-manifest-20202.png',
-	'/img/icon/apple-icon-114x114-dunplab-manifest-20202.png',
-	'/img/icon/favicon-96x96-dunplab-manifest-20202.png',
-	'/img/icon/apple-icon-76x76-dunplab-manifest-20202.png',
-	'/img/icon/apple-icon-72x72-dunplab-manifest-20202.png',
-	'/img/icon/apple-icon-60x60-dunplab-manifest-20202.png',
-	'/img/icon/apple-icon-57x57-dunplab-manifest-20202.png',
-	'/img/icon/favicon-32x32-dunplab-manifest-20202.png',
-	'/img/icon/favicon-16x16-dunplab-manifest-20202.png',
-	'/img/epl-club-alt.png',
-	'https://fonts.googleapis.com/icon?family=Material+Icons&display=swap',
-	'https://fonts.gstatic.com/s/materialicons/v67/flUhRq6tzZclQEJ-Vdg-IuiaDsNcIhQ8tQ.woff2',
-	'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.9.0/css/all.min.css',
-	'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.9.0/webfonts/fa-brands-400.woff2',
-	'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.9.0/webfonts/fa-solid-900.woff2',
-	'/manifest.json',
-];
-
-self.addEventListener('install', (event) => {
-	event.waitUntil(
-		caches.open(CACHE_NAME)
-			.then((cache) => cache.addAll(urlsToCache)),
+importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.1.4/workbox-sw.js');
+ 
+if (workbox){
+	console.log('Workbox berhasil dimuat');
+	workbox.precaching.precacheAndRoute([
+		{ url: '/', revision: '1' },
+		{ url: '/nav.html', revision: '1' },
+		{ url: '/index.html', revision: '1' },
+		{ url: '/teamDetails.html', revision: '1' },
+		{ url: '/pages/home.html', revision: '1' },
+		{ url: '/pages/about.html', revision: '1' },
+		{ url: '/pages/favorites.html', revision: '1' },
+		{ url: '/js/materialize.min.js', revision: '1' },
+		{ url: '/js/nav.js', revision: '1' },
+		{ url: '/js/notification.js', revision: '1' },
+		{ url: '/js/pageLoader.js', revision: '1' },
+		{ url: '/js/register_sw.js', revision: '1' },
+		{ url: '/js/saved.js', revision: '1' },
+		{ url: '/js/standingsApi.js', revision: '1' },
+		{ url: '/js/teamDetails.js', revision: '1' },
+		{ url: '/js/teamDetailsApi.js', revision: '1' },
+		{ url: '/js/db.js', revision: '1' },
+		{ url: '/js/idb.js', revision: '1' },
+		{ url: '/css/materialize.min.css', revision: '1' },
+		{ url: '/css/styles.css', revision: '1' },
+		{ url: '/css/arrow.css', revision: '1' },
+		{ url: '/img/bg-landing.jpg', revision: '1' },
+		{ url: '/img/icon/logo-512x512.png', revision: '1' },
+		{ url: '/img/icon/android-icon-192x192-dunplab-manifest-20202.png', revision: '1' },
+		{ url: '/img/icon/apple-icon-180x180-dunplab-manifest-20202.png', revision: '1' },
+		{ url: '/img/icon/apple-icon-152x152-dunplab-manifest-20202.png', revision: '1' },
+		{ url: '/img/icon/apple-icon-144x144-dunplab-manifest-20202.png', revision: '1' },
+		{ url: '/img/icon/apple-icon-120x120-dunplab-manifest-20202.png', revision: '1' },
+		{ url: '/img/icon/apple-icon-114x114-dunplab-manifest-20202.png', revision: '1' },
+		{ url: '/img/icon/favicon-96x96-dunplab-manifest-20202.png', revision: '1' },
+		{ url: '/img/icon/apple-icon-76x76-dunplab-manifest-20202.png', revision: '1' },
+		{ url: '/img/icon/apple-icon-72x72-dunplab-manifest-20202.png', revision: '1' },
+		{ url: '/img/icon/apple-icon-60x60-dunplab-manifest-20202.png', revision: '1' },
+		{ url: '/img/icon/apple-icon-57x57-dunplab-manifest-20202.png', revision: '1' },
+		{ url: '/img/icon/favicon-32x32-dunplab-manifest-20202.png', revision: '1' },
+		{ url: '/img/icon/favicon-16x16-dunplab-manifest-20202.png', revision: '1' },
+		{ url: '/img/epl-club-alt.png', revision: '1' },
+		{ url: 'https://fonts.googleapis.com/icon?family=Material+Icons&display=swap', revision: '1' },
+		{ url: 'https://fonts.gstatic.com/s/materialicons/v67/flUhRq6tzZclQEJ-Vdg-IuiaDsNc.woff2', revision: '1' },
+		{ url: 'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.9.0/css/all.min.css', revision: '1' },
+		{ url: 'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.9.0/webfonts/fa-brands-400.woff2', revision: '1' },
+		{ url: 'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.9.0/webfonts/fa-solid-900.woff2', revision: '1' },
+		{ url: '/manifest.json', revision: '1' },
+	],
+	{
+		ignoreURLParametersMatching: [/.*/]
+	});
+	workbox.routing.registerRoute(
+		new RegExp('/pages/'),
+		new workbox.strategies.StaleWhileRevalidate({
+			cacheName: 'epl-update-pages',
+		})
 	);
-});
-
-self.addEventListener('activate', (event) => {
-	event.waitUntil(
-		caches.keys()
-			.then((cacheNames) => Promise.all(
-				cacheNames.map((cacheName) => {
-					if (cacheName != CACHE_NAME) {
-						console.log(`ServiceWorker: cache ${cacheName} dihapus`);
-						return caches.delete(cacheName);
-					}
-				}),
-			)),
+	workbox.routing.registerRoute(
+		new RegExp('https://api.football-data.org/v2'),
+		new workbox.strategies.StaleWhileRevalidate({
+			cacheName: 'api',
+		})
 	);
-});
+	workbox.routing.registerRoute(
+		new RegExp('teamDetails.html'),
+		new workbox.strategies.StaleWhileRevalidate({
+			cacheName: 'epl-team-details',
+		})
+	);
+}
+else{
+	console.log('Workbox gagal dimuat');
+	const CACHE_NAME = 'eplupdate';
+	const urlsToCache = [
+		'/',
+		'/nav.html',
+		'/index.html',
+		'/teamDetails.html',
+		'/pages/home.html',
+		'/pages/about.html',
+		'/pages/favorites.html',
+		'/js/materialize.min.js',
+		'/js/nav.js',
+		'/js/notification.js',
+		'/js/pageLoader.js',
+		'/js/register_sw.js',
+		'/js/saved.js',
+		'/js/standingsApi.js',
+		'/js/teamDetails.js',
+		'/js/teamDetailsApi.js',
+		'/js/db.js',
+		'/js/idb.js',
+		'/css/materialize.min.css',
+		'/css/styles.css',
+		'/css/arrow.css',
+		'/img/bg-landing.jpg',
+		'/img/icon/logo-512x512.png',
+		'/img/icon/android-icon-192x192-dunplab-manifest-20202.png',
+		'/img/icon/apple-icon-180x180-dunplab-manifest-20202.png',
+		'/img/icon/apple-icon-152x152-dunplab-manifest-20202.png',
+		'/img/icon/apple-icon-144x144-dunplab-manifest-20202.png',
+		'/img/icon/apple-icon-120x120-dunplab-manifest-20202.png',
+		'/img/icon/apple-icon-114x114-dunplab-manifest-20202.png',
+		'/img/icon/favicon-96x96-dunplab-manifest-20202.png',
+		'/img/icon/apple-icon-76x76-dunplab-manifest-20202.png',
+		'/img/icon/apple-icon-72x72-dunplab-manifest-20202.png',
+		'/img/icon/apple-icon-60x60-dunplab-manifest-20202.png',
+		'/img/icon/apple-icon-57x57-dunplab-manifest-20202.png',
+		'/img/icon/favicon-32x32-dunplab-manifest-20202.png',
+		'/img/icon/favicon-16x16-dunplab-manifest-20202.png',
+		'/img/epl-club-alt.png',
+		'https://fonts.googleapis.com/icon?family=Material+Icons&display=swap',
+		'https://fonts.gstatic.com/s/materialicons/v67/flUhRq6tzZclQEJ-Vdg-IuiaDsNc.woff2',
+		'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.9.0/css/all.min.css',
+		'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.9.0/webfonts/fa-brands-400.woff2',
+		'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.9.0/webfonts/fa-solid-900.woff2',
+		'/manifest.json',
+	];
 
-self.addEventListener('fetch', function(event) {
-	const baseUrl = 'https://api.football-data.org/v2';
-
-
-	if (event.request.url.indexOf(baseUrl) > -1) {
-		event.respondWith(
-			caches.open(CACHE_NAME).then(function(cache) {
-				return fetch(event.request).then(function(response) {
-					cache.put(event.request.url, response.clone());
-					return response;
-				})
-			})
+	self.addEventListener('install', (event) => {
+		event.waitUntil(
+			caches.open(CACHE_NAME)
+				.then((cache) => cache.addAll(urlsToCache)),
 		);
-	} else {
-		event.respondWith(
-			caches.match(event.request, { ignoreSearch: true }).then(function(response) {
-				return response || fetch (event.request);
-			})
-		)
-	}
-});
+	});
+
+	self.addEventListener('activate', (event) => {
+		event.waitUntil(
+			caches.keys()
+				.then((cacheNames) => Promise.all(
+					cacheNames.map((cacheName) => {
+						if (cacheName != CACHE_NAME) {
+							console.log(`ServiceWorker: cache ${cacheName} dihapus`);
+							return caches.delete(cacheName);
+						}
+					}),
+				)),
+		);
+	});
+
+	self.addEventListener('fetch', function(event) {
+		const baseUrl = 'https://api.football-data.org/v2';
+
+
+		if (event.request.url.indexOf(baseUrl) > -1) {
+			event.respondWith(
+				caches.open(CACHE_NAME).then(function(cache) {
+					return fetch(event.request).then(function(response) {
+						cache.put(event.request.url, response.clone());
+						return response;
+					})
+				})
+			);
+		} else {
+			event.respondWith(
+				caches.match(event.request, { ignoreSearch: true }).then(function(response) {
+					return response || fetch (event.request);
+				})
+			)
+		}
+	});
+}
 
 self.addEventListener('push', function (event) {
 	let body;
